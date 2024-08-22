921c27e6373a55f8e2277f7c6e4d4e6786677efa
futurGH <me@futuristick.ca>
2024-08-21 23:20:32 -0400

    emitEvent implementation

--- src/LabelerServer.ts
+++ src/LabelerServer.ts
@@ -1,8 +1,21 @@
-import type { ComAtprotoLabelDefs } from "@atproto/api";
+import {
+	ComAtprotoLabelDefs,
+	ToolsOzoneModerationDefs,
+	ToolsOzoneModerationEmitEvent,
+} from "@atproto/api";
 import { Keypair, Secp256k1Keypair } from "@atproto/crypto";
-import { ErrorFrame, InvalidRequestError, MessageFrame, XRPCError } from "@atproto/xrpc-server";
+import { IdResolver } from "@atproto/identity";
+import {
+	AuthRequiredError,
+	ErrorFrame,
+	InvalidRequestError,
+	MessageFrame,
+	parseReqNsid,
+	verifyJwt,
+	XRPCError,
+} from "@atproto/xrpc-server";
 import Database, { type Database as SQLiteDatabase } from "better-sqlite3";
-import express from "express";
+import express, { type RequestHandler } from "express";
 import expressWs, { type Application, WebsocketRequestHandler } from "express-ws";
 import { Server } from "node:http";
 import { fromString as ui8FromString } from "uint8arrays";
@@ -13,6 +26,7 @@ import { SignedLabel } from "./util/types.js";
 export interface LabelerOptions {
 	did: string;
 	signingKey: string;
+	auth?: (did: string) => boolean | Promise<boolean>;
 	dbFile?: string;
 }
 
@@ -25,13 +39,18 @@ export class LabelerServer {
 
 	did: string;
 
+	private auth: (did: string) => boolean | Promise<boolean>;
+
 	private signingKey: Keypair;
+
+	private idResolver = new IdResolver();
 
 	private subscriptions = new Set<WebSocket>();
 
 	constructor(options: LabelerOptions) {
 		this.did = options.did;
 		this.signingKey = new Secp256k1Keypair(ui8FromString(options.signingKey, "hex"), false);
+		this.auth = options.auth ?? ((did) => did === this.did);
 
 		this.db = new Database(options.dbFile ?? "labels.db");
 		this.db.pragma("journal_mode = WAL");
@@ -52,6 +71,7 @@ export class LabelerServer {
 		this.app = expressWs(express().use(express.json())).app;
 		this.app.get("/xrpc/com.atproto.label.queryLabels", this.queryLabelsHandler);
 		this.app.ws("/xrpc/com.atproto.label.subscribeLabels", this.subscribeLabelsHandler);
+		this.app.post("/xrpc/tools.ozone.moderation.emitEvent", this.emitEventHandler);
 	}
 
 	start(port = 443, callback?: () => void) {
@@ -69,8 +89,45 @@ export class LabelerServer {
 			VALUES (?, ?, ?, ?, ?, ?, ?, ?)
 		`);
 		const { src, uri, cid, val, neg, cts, exp, sig } = signed;
-		stmt.run(src, uri, cid, val, neg, cts, exp, sig);
+		const result = stmt.run(src, uri, cid, val, neg, cts, exp, sig);
+		if (!result.changes) throw new Error("Failed to insert label");
+		await this.emitLabel(signed);
 		return signed;
+	}
+
+	async createLabels(
+		subject: { uri: string; cid?: string | undefined },
+		labels: { create?: Array<string>; negate?: Array<string> },
+	) {
+		const { uri, cid } = subject;
+		const { create, negate } = labels;
+		if (create) {
+			await Promise.all(
+				create.map((val) =>
+					this.createLabel({
+						src: this.did,
+						uri,
+						...(cid ? { cid } : {}),
+						val,
+						cts: new Date().toISOString(),
+					})
+				),
+			);
+		}
+		if (negate) {
+			await Promise.all(
+				negate.map((val) =>
+					this.createLabel({
+						src: this.did,
+						uri,
+						...(cid ? { cid } : {}),
+						val,
+						neg: true,
+						cts: new Date().toISOString(),
+					})
+				),
+			);
+		}
 	}
 
 	private async ensureSignedLabel(label: ComAtprotoLabelDefs.Label): Promise<SignedLabel> {
@@ -87,7 +144,33 @@ export class LabelerServer {
 		return formatLabel(label);
 	}
 
-	queryLabelsHandler: express.RequestHandler = async (req, res) => {
+	private async emitLabel(label: ComAtprotoLabelDefs.Label) {
+		const signed = await this.ensureSignedLabel(label);
+		const frame = new MessageFrame({ seq: label.id, labels: [signed] }, { type: "#labels" });
+		this.subscriptions.forEach((ws) => ws.send(frame.toBytes()));
+	}
+
+	private async parseAuthHeaderDid(req: express.Request): Promise<string> {
+		const authHeader = req.get("Authorization");
+		if (!authHeader) throw new AuthRequiredError("Authorization header is required");
+
+		const [type, token] = authHeader.split(" ");
+		if (type !== "Bearer" || !token) {
+			throw new InvalidRequestError("Missing or invalid bearer token", "MissingJwt");
+		}
+
+		const payload = await verifyJwt(
+			token,
+			this.did,
+			parseReqNsid(req),
+			async (did, forceRefresh) =>
+				(await this.idResolver.did.resolveAtprotoData(did, forceRefresh)).signingKey,
+		);
+
+		return payload.iss;
+	}
+
+	queryLabelsHandler: RequestHandler = async (req, res) => {
 		try {
 			const { uriPatterns, sources, limit: limitStr, cursor: cursorStr } = req.query as {
 				uriPatterns?: Array<string>;
@@ -139,7 +222,7 @@ export class LabelerServer {
 				console.error(e);
 				res.status(500).json({
 					error: "InternalError",
-					message: e instanceof Error ? e.message : "An unknown error occurred",
+					message: "An unknown error occurred",
 				});
 			}
 			return;
@@ -181,5 +264,59 @@ export class LabelerServer {
 		ws.on("close", () => {
 			this.subscriptions.delete(ws);
 		});
+	};
+
+	emitEventHandler: RequestHandler = async (req, res) => {
+		try {
+			const actorDid = await this.parseAuthHeaderDid(req);
+			const authed = await this.auth(actorDid);
+			if (!authed) {
+				throw new AuthRequiredError("Unauthorized");
+			}
+
+			const { event, subject, createdBy } = req
+				.body as ToolsOzoneModerationEmitEvent.InputSchema;
+			if (!event || !subject || !createdBy) {
+				throw new InvalidRequestError("Missing required field(s)");
+			}
+
+			if (event.$type !== "tools.ozone.moderation.defs#modEventLabel") {
+				throw new InvalidRequestError("Unsupported event type");
+			}
+			const labelEvent = event as ToolsOzoneModerationDefs.ModEventLabel;
+			const uri =
+				subject.$type === "com.atproto.admin.defs#repoRef"
+					&& typeof subject.did === "string"
+					? subject.did
+					: (subject.$type === "com.atproto.repo.strongRef#main"
+							|| subject.$type === "com.atproto.repo.strongRef")
+							&& typeof subject.uri === "string"
+					? subject.uri
+					: null;
+			const cid =
+				subject.$type === "com.atproto.admin.defs#repoRef"
+					&& typeof subject.cid === "string"
+					? subject.cid
+					: undefined;
+
+			if (!uri) {
+				throw new InvalidRequestError("Invalid subject");
+			}
+
+			await this.createLabels({ uri, cid }, {
+				create: labelEvent.createLabelVals,
+				negate: labelEvent.negateLabelVals,
+			});
+		} catch (e) {
+			if (e instanceof XRPCError) {
+				res.status(e.type).json(e.payload);
+			} else {
+				console.error(e);
+				res.status(500).json({
+					error: "InternalError",
+					message: "An unknown error occurred",
+				});
+			}
+		}
 	};
 }
