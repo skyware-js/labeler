33b4d456bf187da9ded94abc9704a45aeaa33398
futur <me@futuristick.ca>
2024-08-27 10:14:32 -0400

    Export everything

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { type LabelerOptions, LabelerServer } from "./LabelerServer.js";
 export { formatLabel, labelIsSigned, signLabel } from "./util/labels.js";
-export type { SignedLabel } from "./util/types.js";
+export type { GetMethod, SavedLabel, SignedLabel, WebSocketMethod } from "./util/types.js";
